df68877f9c71642f5f4f633780ed3901ba97af3c
nin-jin <nin-jin@users.noreply.github.com>
2024-12-29 07:09:25 +0000

    Deploying to gh-pages from @ hyoo-ru/meme.hyoo.ru@f7a6ade567aa44c0928aad0ff0664d62eeffb810 🚀

--- node.test.js
+++ node.test.js
@@ -3752,7 +3752,7 @@ var $;
             Tools: {
                 flex: {
                     basis: 'auto',
-                    grow: 1000,
+                    grow: 0,
                     shrink: 1,
                 },
                 display: 'flex',

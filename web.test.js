9c139b418b1a78a86b2730ce39ef96ff43b860d9
nin-jin <nin-jin@users.noreply.github.com>
2025-07-04 07:13:56 +0000

    Deploying to gh-pages from @ hyoo-ru/meme.hyoo.ru@a2014762a334093d262916ff0e0384538b528173 🚀

--- web.test.js
+++ web.test.js
@@ -1672,8 +1672,6 @@ var $;
                         $mol_wire_sync(this).wait();
                     return this.value();
                 }
-                static test() {
-                }
             }
             __decorate([
                 $mol_wire_solo
@@ -1684,9 +1682,6 @@ var $;
             __decorate([
                 $mol_wire_solo
             ], App, "result", null);
-            __decorate([
-                $mol_wire_method
-            ], App, "test", null);
             $mol_assert_equal(App.result(), 1);
             App.resets(null);
             $mol_wire_fiber.sync();

05348cd09c96d5234b7dc2f349318de2e805387f
nin-jin <nin-jin@users.noreply.github.com>
2025-09-09 07:12:05 +0000

    Deploying to gh-pages from @ hyoo-ru/meme.hyoo.ru@a2014762a334093d262916ff0e0384538b528173 🚀

--- node.mjs
+++ node.mjs
@@ -6805,6 +6805,7 @@ var $;
 			return {
 				...(super.attr()), 
 				"src": (this.uri()), 
+				"title": (this.hint()), 
 				"alt": (this.title()), 
 				"loading": (this.loading()), 
 				"decoding": (this.decoding()), 
--- node.test.js
+++ node.test.js
@@ -6796,6 +6796,7 @@ var $;
 			return {
 				...(super.attr()), 
 				"src": (this.uri()), 
+				"title": (this.hint()), 
 				"alt": (this.title()), 
 				"loading": (this.loading()), 
 				"decoding": (this.decoding()), 

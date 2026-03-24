d6afaa72086fb5295fccbc50bf73561ae0bb60ea
nin-jin <nin-jin@users.noreply.github.com>
2026-03-24 07:48:42 +0000

    Deploying to gh-pages from @ hyoo-ru/meme.hyoo.ru@a2014762a334093d262916ff0e0384538b528173 🚀

--- node.mjs
+++ node.mjs
@@ -7297,6 +7297,7 @@ var $;
 "use strict";
 var $;
 (function ($) {
+    let x = /x/[Symbol.matchAll];
     class $mol_regexp extends RegExp {
         groups;
         constructor(source, flags = 'gsu', groups = []) {
--- node.test.js
+++ node.test.js
@@ -7288,6 +7288,7 @@ var $;
 "use strict";
 var $;
 (function ($) {
+    let x = /x/[Symbol.matchAll];
     class $mol_regexp extends RegExp {
         groups;
         constructor(source, flags = 'gsu', groups = []) {
